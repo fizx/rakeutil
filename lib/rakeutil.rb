bff1d144330f448f5c14242f8ac66a8e3f7356b7
Kyle Maxwell <kyle@casttv.com>
2008-08-20 14:43:13 -0700

    basic project is here

---
+++ lib/rakeutil.rb
@@ -0,0 +1,71 @@
+require "fileutils"
+include FileUtils::Verbose
+require "rubygems"
+require "ostruct"
+
+def const(k, v)
+  k = k.to_s
+  $rake_consts ||= []
+  $rake_consts << k
+  Kernel.const_set(k, ENV[k] || v)
+end
+
+def system?(cmd)
+  puts cmd
+  Kernel.system(cmd) or exit unless $dry
+end
+
+def system(cmd)
+  puts cmd
+  Kernel.system(cmd) unless $dry
+end
+
+def jx(*args)
+  system "java -cp #{RAKE_CLASSPATH} #{RAKE_JAVA_OPTS} #{args.join(" ")}"
+end
+
+const :RAKE_CLASSPATH, (Dir["**/*.jar"] << "bin").join(":")
+const :RAKE_JAVA_OPTS, " -d64 -Xmx14g "
+
+class Object
+  def to_os
+    self
+  end
+end
+
+class Hash
+  def to_os
+    inject({}){ |memo, (k, v)| memo[k] = v.to_os; memo }
+  end
+end
+
+class Array
+  def to_os
+    map{|e| e.to_os }
+  end
+end
+
+
+namespace :util do
+
+  desc "cat rakeutil's README"
+  task :readme do
+    puts File.dirname(__FILE__) + "/../README"
+  end
+  
+  desc "Don't let fileutils and compatible commands affect the fs"
+  task :dry => :dry_run do
+  end
+  
+  desc "Don't let fileutils and compatible commands affect the fs"
+  task :dry_run do
+    include FileUtils::DryRun
+    $dry = true
+  end
+
+  desc "Pretty-print all rakeutil constants"
+  task :constants do
+    $rake_consts.sort.each {|c| puts c + "\t" + const_get(c).inspect }
+  end
+  
+end
---
+++ rakeutil.gemspec
@@ -0,0 +1,28 @@
+require 'rubygems'
+ 
+spec = Gem::Specification.new do |s|
+  s.name = 'rakeutil'
+  s.version = "1.0.0"
+ 
+  s.author = "CastTV"
+  s.email = "kyle@casttv.com"
+  s.homepage = "http://www.casttv.com/"
+  s.summary = "Rake Utilities"
+  s.description = "Making my life easier"
+ 
+  s.platform = Gem::Platform::RUBY
+  s.files = Dir.glob("{bin,doc,etc,lib,sample,test}/**/*").delete_if {|item| item.include?("CVS") || item.include?("rdoc")}
+  s.files.concat ["README", "README.DEV", "ChangeLog"]
+ 
+  # s.rdoc_options << '--exclude' << '.'
+  # s.has_rdoc = false
+ 
+  s.require_path = 'lib'
+  s.autorequire = 'rakeutil'
+ 
+end
+ 
+if $0 == __FILE__
+  Gem::manage_gems
+  Gem::Builder.new(spec).build
+end
