0656ef581c305c732fe32ec1c34e5402e1b07216
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2008-10-22 14:25:45 -0700

    fixed bug

--- lib/rakeutil.rb
+++ lib/rakeutil.rb
@@ -53,26 +53,24 @@ def shell_script_test(files)
   succeeded = 0
   failed = 0
   
-  log_file = Module.const_defined?(TEST_LOG) ? File.open(TEST_LOG, "w") : STDER
-  
-  log_file do |log|
-    files.each do |file|
-      log.puts `#{file} 2>&1`
-      if $?.exitstatus == 0
-        print "."
-        STDOUT.flush
-        succeeded += 1
-      else
-        print "F"
-        STDOUT.flush
-        failed += 1
-      end
-    end  
+  log = Kernel.const_defined?(:TEST_LOG) ? File.open(TEST_LOG, "w") : STDER
+  files.each do |file|
+    log.puts `#{file} 2>&1`
+    if $?.exitstatus == 0
+      print "."
+      STDOUT.flush
+      succeeded += 1
+    else
+      print "F"
+      STDOUT.flush
+      failed += 1
+    end
   end
   
   total = succeeded + failed
   puts
   puts "#{succeeded} of #{total} tests succeeded"
+  log.close
   exit succeeded == total ? 0 : 1
 end
 
